19b3aab840dea13872438b72a32b6721744d251d
hanyangii <id9595@hanyang.ac.kr>
2018-03-21 15:10:51 +0000

    loss function fix

--- code/LstmModel.py
+++ code/LstmModel.py
@@ -122,7 +122,7 @@ class LstmModel:
         self.logits = tf.dynamic_partition(logits, self.target_ids, 2)[1]
         logits2 = tf.dynamic_partition(logits, self.target_ids, 2)[0]
 
-        #####From Here######
+       
         self.predictions = tf.round(tf.nn.sigmoid(self.logits))
         logit_dic = {'logits':self.logits, 'logit2':logits2, 'target_ids':self.target_ids, 'target':self.targets, 'prediction':self.predictions, 'original_logits':original_logits}
         self.logit_list.append(logit_dic)
@@ -139,10 +139,10 @@ class LstmModel:
         https://www.tensorflow.org/versions/r0.12/api_docs/python/train
         /gradient_clipping
         """
-        loss_per_example = tf.nn.softmax_cross_entropy_with_logits(
+        loss_per_example = tf.nn.sigmoid_cross_entropy_with_logits(
             logits=self.logits, labels=self.targets)
         self.loss = tf.reduce_mean(loss_per_example)
-        #####To Here######
+        
         self.summary_loss = [tf.summary.scalar('loss', self.loss)]
         # track number of batches seen
         self.global_step = tf.Variable(0, name="global_step", trainable=False)
