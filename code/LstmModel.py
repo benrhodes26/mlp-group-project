fix AUC calculation and parameter reuse, and tidy up the code

code/LstmModel.py
@@ -7,7 +7,7 @@ class LstmModel:
         return "LstmModel"
 
     def __init__(self, max_time_steps=973, feature_len=293,
-                 n_distinct_questions=146, var_dropout=True, batch_size = 32):
+                 n_distinct_questions=146, var_dropout=True, batch_size=32):
         """Initialise task-specific parameters."""
         self.max_time_steps = max_time_steps
         self.feature_len = feature_len
@@ -17,25 +17,15 @@ class LstmModel:
         self.auc_init = None
         self.summary_loss = None
         self.summary_aucacc = None
-        self.logit_list = []
         self.batch_size = batch_size
+        self.reuse = False
 
-    def build_graph(
-            self,
-            n_hidden_layers=1,
-            n_hidden_units=200,
-            clip_norm=5*1e-5,
-            add_gradient_noise=1e-3,
-            optimisation='adam'):
-
-        self._build_model(n_hidden_layers=n_hidden_layers,
-                          n_hidden_units=n_hidden_units)
-        self._build_training(clip_norm=clip_norm,
-                             add_gradient_noise=add_gradient_noise,
-                             optimisation=optimisation)
+    def build_graph(self, n_hidden_units=200, clip_norm=5*1e-5, optimisation='adam'):
+        self._build_model(n_hidden_units=n_hidden_units)
+        self._build_training(clip_norm=clip_norm, optimisation=optimisation)
         self._build_metrics()
 
-    def _build_model(self, n_hidden_layers=1, n_hidden_units=200):
+    def _build_model(self, n_hidden_units=200):
         """Build a TensorFlow computational graph for an LSTM network.
 
         Model based on "DKT paper" (see section 3):
@@ -46,13 +36,13 @@ class LstmModel:
             Xiong, Xiaolu, et al. "Going Deeper with Deep Knowledge Tracing."
             EDM. 2016.
 
-
         Parameters
         ----------
-        n_hidden_layers : int (default=1)
-            A single hidden layer was used in DKT paper
         n_hidden_units : int (default=200)
             200 hidden units were used in DKT paper
+        is_training: bool (default=True)
+            if False, we are evaluating on validation set, so reuse
+            RNN parameters from training phase
         """
         tf.reset_default_graph()
 
@@ -74,104 +64,55 @@ class LstmModel:
 
         self.keep_prob = tf.placeholder_with_default(1.0, shape=(),
                                                      name='keep_prob')
-        # with tf.variable_scope('RNN', initializer=tf.contrib.layers.xavier_initializer()):
-        # todo worry about initialisation?
-        with tf.variable_scope('RNN', initializer=tf.random_uniform_initializer(-0.005, 0.005)):
-            # model. LSTM layer(s) then linear layer (softmax applied in loss)
 
-            if n_hidden_layers == 1:
-                cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units)
+        with tf.variable_scope('RNN', reuse=self.reuse,
+                               initializer=tf.random_uniform_initializer(-0.05, 0.05)):
 
-                if self.var_dropout:
-                    cell = tf.nn.rnn_cell.DropoutWrapper(cell,
-                                                         output_keep_prob=self.keep_prob,
-                                                         state_keep_prob=self.keep_prob,
-                                                         variational_recurrent=self.var_dropout,
-                                                         dtype=tf.float32)
-                else:
-                    # Only apply non-variational dropout to output connections
-                    cell = tf.nn.rnn_cell.DropoutWrapper(cell,
-                                                         output_keep_prob=self.keep_prob,
-                                                         dtype=tf.float32)
-
-                self.outputs, self.state = tf.nn.dynamic_rnn(cell=cell,
-                                                             inputs=self.inputs,
-                                                             dtype=tf.float32,
-                                                             initial_state=cell.zero_state(batch_size=self.batch_size,
-                                                                                           dtype=tf.float32))
-
+            cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units)
+            if self.var_dropout:
+                # Apply variational dropout to recurrent state and output
+                cell = tf.nn.rnn_cell.DropoutWrapper(cell,
+                                                     output_keep_prob=self.keep_prob,
+                                                     state_keep_prob=self.keep_prob,
+                                                     variational_recurrent=self.var_dropout,
+                                                     dtype=tf.float32)
             else:
-                init_state = tf.placeholder(tf.float32,
-                                            [n_hidden_layers, 2, self.batch_size, n_hidden_units])
-                state_per_layer_list = tf.unpack(init_state, axis=0)
-                state_list = [tf.nn.rnn_cell.LSTMStateTuple(state_per_layer_list[i][0],
-                                                            state_per_layer_list[i][1])
-                                                            for i in range(n_hidden_layers)]
-                rnn_tuple_state = tuple(state_list)
+                # Apply non-variational dropout to output
+                cell = tf.nn.rnn_cell.DropoutWrapper(cell,
+                                                     output_keep_prob=self.keep_prob,
+                                                     dtype=tf.float32)
 
-
-                cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, state_is_tuple=True)
-                if self.var_dropout:
-                    cell = tf.nn.rnn_cell.DropoutWrapper(cell,
-                                                         output_keep_prob=self.keep_prob,
-                                                         state_keep_prob=self.keep_prob,
-                                                         variational_recurrent=self.var_dropout,
+            self.outputs, self.state = tf.nn.dynamic_rnn(cell=cell,
+                                                         inputs=self.inputs,
                                                          dtype=tf.float32)
-                else:
-                    # Only apply non-variational dropout to output connections
-                    cell = tf.nn.rnn_cell.DropoutWrapper(cell,
-                                                         output_keep_prob=self.keep_prob,
-                                                         dtype=tf.float32)
-                cells = [cell for layer in range(n_hidden_layers)]
-                cell = tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
-                self.outputs, self.state = tf.nn.dynamic_rnn(cell=cell,
-                                                             inputs=self.inputs,
-                                                             dtype=tf.float32,
-                                                             initial_state=rnn_tuple_state)
 
+            sigmoid_w = tf.get_variable(dtype=tf.float32,
+                                        name="sigmoid_w",
+                                        shape=[n_hidden_units,
+                                               self.n_distinct_questions])
+            sigmoid_b = tf.get_variable(dtype=tf.float32,
+                                        name="sigmoid_b",
+                                        shape=[self.n_distinct_questions])
 
-        sigmoid_w = tf.get_variable(dtype=tf.float32,
-                                    name="sigmoid_w",
-                                    shape=[n_hidden_units,
-                                           self.n_distinct_questions])
-        sigmoid_b = tf.get_variable(dtype=tf.float32,
-                                    name="sigmoid_b",
-                                    shape=[self.n_distinct_questions])
+            # make first dim batch_size times max_time_steps
+            self.outputs = tf.reshape(self.outputs,
+                                      shape=[-1, n_hidden_units])
 
-        # reshaping as done in GD paper code
-        # first dim now batch_size times max_time_steps
-        self.outputs = tf.reshape(self.outputs,
-                                  shape=[-1, n_hidden_units])
+            logits = tf.matmul(self.outputs, sigmoid_w) + sigmoid_b
+            logits = tf.reshape(logits, [-1])
+            self.logits = tf.dynamic_partition(logits, self.target_ids, 2)[1]
 
-        logits = tf.matmul(self.outputs, sigmoid_w) + sigmoid_b
-        logits = tf.reshape(logits, [-1])
+            loss_per_example = tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.logits, labels=self.targets)
+            self.loss = tf.reduce_mean(loss_per_example)
+            self.summary_loss = [tf.summary.scalar('loss', self.loss)]
 
-        original_logits =logits
-        self.logits = tf.dynamic_partition(logits, self.target_ids, 2)[1]
-        logits2 = tf.dynamic_partition(logits, self.target_ids, 2)[0]
+            # need predictions to calculate accuracy and auc
+            self.predictions = tf.nn.sigmoid(self.logits)
 
-       
-        self.predictions = tf.round(tf.nn.sigmoid(self.logits))
-        logit_dic = {'logits':self.logits, 'logit2':logits2, 'target_ids':self.target_ids, 'target':self.targets, 'prediction':self.predictions, 'original_logits':original_logits}
-        self.logit_list.append(logit_dic)
+    def _build_training(self, clip_norm=5*1e-5, optimisation='adam'):
+        """Define parameters updates."""
 
-    def _build_training(self, clip_norm=5*1e-5, add_gradient_noise=1e-3,
-                        optimisation='adam'):
-        """Define parameters updates.
-
-        Applies exponential learning rate decay (optional). See:
-        https://www.tensorflow.org/versions/r0.12/api_docs/python/train
-        /decaying_the_learning_rate
-
-        Applies gradient clipping by global norm (optional). See:
-        https://www.tensorflow.org/versions/r0.12/api_docs/python/train
-        /gradient_clipping
-        """
-        loss_per_example = tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.logits, labels=self.targets)
-        self.loss = tf.reduce_mean(loss_per_example)
-        
-        self.summary_loss = [tf.summary.scalar('loss', self.loss)]
         # track number of batches seen
         self.global_step = tf.Variable(0, name="global_step", trainable=False)
 
@@ -180,8 +121,7 @@ class LstmModel:
 
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
-            # Ensures that we execute the update_ops before performing the
-            # train_step
+            # Ensures that we execute the update_ops before performing the train_step
 
             if optimisation == 'adam':
                 optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
@@ -197,8 +137,6 @@ class LstmModel:
             if clip_norm:
                 # grads, _ = tf.clip_by_global_norm(grads, clip_norm)
                 grads = [tf.clip_by_norm(grad, clip_norm) for grad in grads]
-            #if add_gradient_noise:
-            #    grads = [self.add_noise(g) for g in grads]
 
             self.grads_and_vars = list(zip(grads, trainable_vars))
             self.training = optimizer.apply_gradients(
@@ -206,9 +144,9 @@ class LstmModel:
                 global_step=self.global_step)
 
     def _build_metrics(self):
-        """Add ability to compute accuracy and AUC."""
+        """Compute accuracy and AUC."""
         self.accuracy = tf.metrics.accuracy(labels=self.targets,
-                                            predictions=self.predictions,
+                                            predictions=tf.round(self.predictions),
                                             name="acc")
 
         self.auc = tf.metrics.auc(labels=self.targets,
@@ -225,20 +163,3 @@ class LstmModel:
 
         acc_var = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="acc")
         self.acc_init = tf.variables_initializer(var_list=acc_var)
-
-    def add_noise(self, t, stddev=1e-3, name=None):
-        """
-        This code taken directly from:
-            Xiong, Xiaolu, et al. "Going Deeper with Deep Knowledge Tracing."
-            EDM. 2016.
-
-        Adds gradient noise as described in http://arxiv.org/abs/1511.06807 [2].
-        The input Tensor `t` should be a gradient.
-        The output will be `t` + gaussian noise.
-        0.001 was said to be a good fixed value for memory networks [2].
-        """
-        with tf.name_scope(name, "add_gradient_noise", [t, stddev]) as name:
-            t = tf.convert_to_tensor(t, name="t")
-            gn = tf.random_normal(tf.shape(t), stddev=stddev)
-
-        return tf.add(t, gn, name=name)

code/data_provider.py
@@ -321,7 +321,7 @@ class ASSISTDataProvider(DataProvider):
         self.targets = self.targets[perm]
         self.target_ids = self.target_ids[perm]
 
-    def _get_k_folds(self, k, threshold):
+    def _get_k_folds(self, k, threshold=None):
         """ Returns k pairs of DataProviders: (train_data_provider, val_data_provider)
         where the data split in each tuple is determined by k-fold cross val."""
 
@@ -340,16 +340,19 @@ class ASSISTDataProvider(DataProvider):
             targets_train, targets_val = targets[train_index], targets[val_index]
             target_ids_train, targets_ids_val = target_ids[train_index], target_ids[val_index]
 
-            # break up a student's sequence (into threshold-sized chunks)
-            # *after* the train/val split since if we did it beforehand then the same
-            # students' data might be split across the two sets, which would make the
-            # validation set a bad proxy for the test set.
-            inputs_train, target_ids_train, targets_train, threshold = \
-                self.threshold_num_ans(inputs_train, target_ids_train,
-                                       targets_train, threshold)
-            inputs_val, targets_ids_val, targets_val, threshold = \
-                self.threshold_num_ans(inputs_val, targets_ids_val,
-                                       targets_val, threshold)
+            if threshold:
+                # break up a student's sequence (into threshold-sized chunks)
+                # *after* the train/val split since if we did it beforehand then the same
+                # students' data might be split across the two sets, which would make the
+                # validation set a bad proxy for the test set.
+                inputs_train, target_ids_train, targets_train, threshold = \
+                    self.truncate_sequences(inputs_train, target_ids_train,
+                                            targets_train, threshold)
+                inputs_val, targets_ids_val, targets_val, threshold = \
+                    self.truncate_sequences(inputs_val, targets_ids_val,
+                                            targets_val, threshold)
+            else:
+                threshold = self.max_num_ans
 
             train_data = {
                 'inputs': inputs_train,
@@ -392,7 +395,7 @@ class ASSISTDataProvider(DataProvider):
                 data=val_data)
             yield (train_dp, val_dp)
 
-    def train_validation_split(self, threshold):
+    def train_validation_split(self, threshold=None):
         """Return 2 data providers with 80/20 data split
 
         Note, we break up a student's sequence (into threshold-sized chunks)
@@ -405,7 +408,7 @@ class ASSISTDataProvider(DataProvider):
             break
         return train_provider, validation_provider
 
-    def threshold_num_ans(self, inputs, target_ids, targets, threshold):
+    def truncate_sequences(self, inputs, target_ids, targets, threshold):
         """Split the data of each student into threshold*encoding_dim chunks.
 
         Rather than use the default max_num_ans*encoding_dim vector that contains all the
@@ -414,53 +417,54 @@ class ASSISTDataProvider(DataProvider):
         student's data with zeros, some of these new chunks will be all zero, and can thus
         be discarded."""
         threshold = min(self.max_num_ans, threshold)
-        num_students = inputs.shape[0]
-
-        inputs = self._threshold_inputs_or_ids(inputs,
-                                               self.encoding_dim,
-                                               num_students,
-                                               threshold)
-        target_ids = self._threshold_inputs_or_ids(target_ids,
-                                                   self.max_prob_set_id,
-                                                   num_students,
-                                                   threshold)
-        targets = self._threshold_targets(targets, threshold)
+        inputs = self._truncate_inputs_or_ids(inputs,
+                                              self.encoding_dim,
+                                              threshold)
+        target_ids = self._truncate_inputs_or_ids(target_ids,
+                                                  self.max_prob_set_id,
+                                                  threshold)
+        targets = self._truncate_targets(targets, threshold)
         print('Number of effective students is now {}.'.format(inputs.shape[0]))
 
         return inputs, target_ids, targets, threshold
 
-    def _threshold_targets(self, targets, threshold):
+    def _truncate_targets(self, targets, threshold):
         new_targets = []
         for student in targets:
             for i in range(0, len(student), threshold):
                 new_targets.append(student[i:i + threshold])
         return np.array(new_targets)
 
-    def _threshold_inputs_or_ids(self, input_or_id, final_dim,
-                                 num_students, threshold):
-        x = input_or_id.toarray()
-        x = x.reshape(num_students,
-                      self.max_num_ans,
-                      final_dim)
+    def _truncate_inputs_or_ids(self, input_or_id, final_dim, threshold):
 
-        # We want to break the data into threshold-sized chunks,
-        # so right-pad with zeros to ensure divisibility
-        pad_size = threshold - self.max_num_ans % threshold
-        pad = ((0, 0), (0, pad_size), (0, 0))
-        x = np.pad(x, pad_width=pad, mode='constant', constant_values=0)
+        # don't truncate all the input_or_ids in one-go due to memory overload.
+        # do it in steps of 1000 students and concatenate afterwards
+        list_of_truncated_seqs = []
+        for i in range(0, input_or_id.shape[0], 1000):
+            x = input_or_id[i:i+1000].toarray()
+            x = x.reshape(x.shape[0],
+                          self.max_num_ans,
+                          final_dim)
 
-        # break up data into threshold-sized chunks
-        new_x = x.reshape(-1, threshold, final_dim)
+            # We want to break the data into threshold-sized chunks,
+            # so right-pad with zeros to ensure divisibility
+            pad_size = threshold - self.max_num_ans % threshold
+            pad = ((0, 0), (0, pad_size), (0, 0))
+            x = np.pad(x, pad_width=pad, mode='constant', constant_values=0)
 
-        # Only keep those chunks that are non-zero
-        non_empty_mask = np.max((new_x != np.zeros((threshold, final_dim))),
-                                axis=(1, 2))
-        num_effective_students = np.sum(non_empty_mask)
-        non_empty_indices = np.nonzero(non_empty_mask)
-        new_x = new_x[non_empty_indices]
-        new_x = sp.csr_matrix(new_x.reshape(num_effective_students, -1))
+            # break up data into threshold-sized chunks
+            new_x = x.reshape(-1, threshold, final_dim)
 
-        return new_x
+            # Only keep those chunks that are non-zero
+            non_empty_mask = np.max((new_x != np.zeros((threshold, final_dim))),
+                                    axis=(1, 2))
+            num_effective_students = np.sum(non_empty_mask)
+            non_empty_indices = np.nonzero(non_empty_mask)
+            new_x = new_x[non_empty_indices]
+            list_of_truncated_seqs.append(
+                sp.csr_matrix(new_x.reshape(num_effective_students, -1)))
+
+        return sp.vstack(list_of_truncated_seqs)
 
     def _validate_inputs(self, which_set, which_year, data_path):
         assert which_set in ['train', 'test'], (

code/run_training.py
@@ -1,20 +1,16 @@
 from data_provider import ASSISTDataProvider
 from LstmModel import LstmModel
-from utils import get_events_filepath, events_to_numpy, get_learning_rate
+from utils import get_learning_rate, log_learning_rate_and_grad_norms, plot_learning_curves
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from time import gmtime, strftime
 
 import os
-import numpy as np
 import tensorflow as tf
-import matplotlib
-
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
 
 START_TIME = strftime('%Y%m%d-%H%M', gmtime())
 
+# Arguments for reading and writing data
 parser = ArgumentParser(description='Train LstmModel.',
                         formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--data_dir', type=str,
@@ -26,7 +22,35 @@ parser.add_argument('--which_year', type=str, default='09',
                     help='Year of ASSIST data. Either 09 or 15')
 parser.add_argument('--restore', default=None,
                     help='Path to .ckpt file of model to continue training')
+parser.add_argument('--name', type=str, default=START_TIME,
+                    help='Name of experiment when saving model')
+parser.add_argument('--model_dir', type=str, default='.',
+                    help='Path to directory where model will be saved')
 
+# Arguments for debugging
+parser.add_argument('--log_stats', dest='log_stats', action='store_true',
+                    help='print learning rate and gradient norms once every 10 epochs')
+parser.add_argument('--no-log_stats', dest='log_stats', action='store_false',
+                    help='do not print learning rate and gradient norms once every 10 epochs')
+parser.set_defaults(log_stats=False)
+parser.add_argument('--fraction', type=float, default=1.0,
+                    help='Fraction of data to use. Useful for hyperparameter tuning')
+
+# Arguments controlling feature representation
+parser.add_argument('--plus_minus_feats', dest='plus_minus_feats', action='store_true',
+                    help='use +/- for feature encoding')
+parser.add_argument('--no-plus_minus_feats', dest='plus_minus_feats', action='store_false',
+                    help='do not use +/- for feature encoding')
+parser.set_defaults(plus_minus_feats=False)
+parser.add_argument('--compressed_sensing', dest='compressed_sensing', action='store_true',
+                    help='use compressed sensing')
+parser.add_argument('--no-compressed_sensing', dest='compressed_sensing', action='store_false',
+                    help='do not use use compressed sensing')
+parser.set_defaults(compressed_sensing=False)
+parser.add_argument('--max_time_steps', type=int, default=None,
+                    help='limit length of students sequences of answers')
+
+# Arguments to control model hyperparameters
 parser.add_argument('--optimisation', type=str, default='sgd',
                     help='optimisation method. Choices are: adam, rmsprop, '
                          'momentum and sgd.')
@@ -40,20 +64,10 @@ parser.add_argument('--lr_exp_decay', type=float, default=(1 / 3),
                     help='fraction to multiply learning rate by each step')
 parser.add_argument('--num_hidden_units', type=int, default=200,
                     help='Number of hidden units in the LSTM cell')
-parser.add_argument('--num_hidden_layers', type=int, default=1,
-                    help='Number of hidden layers in the LSTM cell')
 parser.add_argument('--batch', type=int, default=32,
                     help='Batch size')
-parser.add_argument('--max_time_steps', type=int, default=100,
-                    help='limit length of students sequences of answers')
 parser.add_argument('--epochs', type=int, default=100,
                     help='Number of training epochs')
-parser.add_argument('--decay', type=float, default=0.96,
-                    help='Fraction to decay learning rate every 100 batches')
-parser.add_argument('--decay_step', type=int, default=3000,
-                    help='Apply learning rate decay every x batches')
-# parser.add_argument('--add_gradient_noise', type=float, default=1e-3,
-#                    help='add gaussian noise with stdev=1e-3 to gradients')
 parser.add_argument('--clip_norm', type=float, default=1,
                     help='clip norms of gradients')
 parser.add_argument('--keep_prob', type=float, default=0.6,
@@ -63,27 +77,6 @@ parser.add_argument('--var_dropout', dest='var_dropout', action='store_true',
 parser.add_argument('--no-var_dropout', dest='var_dropout', action='store_false',
                     help='do not use variational dropout')
 parser.set_defaults(var_dropout=True)
-parser.add_argument('--plus_minus_feats', dest='plus_minus_feats', action='store_true',
-                    help='use +/- for feature encoding')
-parser.add_argument('--no-plus_minus_feats', dest='plus_minus_feats', action='store_false',
-                    help='do not use +/- for feature encoding')
-parser.set_defaults(plus_minus_feats=False)
-parser.add_argument('--compressed_sensing', dest='compressed_sensing', action='store_true',
-                    help='use compressed sensing')
-parser.add_argument('--no-compressed_sensing', dest='compressed_sensing', action='store_false',
-                    help='do not use use compressed sensing')
-parser.set_defaults(compressed_sensing=False)
-parser.add_argument('--log_stats', dest='log_stats', action='store_true',
-                    help='print learning rate and gradient norms once every 10 epochs')
-parser.add_argument('--no-log_stats', dest='log_stats', action='store_false',
-                    help='do not print learning rate and gradient norms once every 10 epochs')
-parser.set_defaults(log_stats=False)
-parser.add_argument('--fraction', type=float, default=1.0,
-                    help='Fraction of data to use. Useful for hyperparameter tuning')
-parser.add_argument('--name', type=str, default=START_TIME,
-                    help='Name of experiment when saving model')
-parser.add_argument('--model_dir', type=str, default='.',
-                    help='Path to directory where model will be saved')
 args = parser.parse_args()
 
 SAVE_DIR = os.path.join(args.model_dir, args.name)
@@ -99,28 +92,24 @@ data_provider = ASSISTDataProvider(
     fraction=args.fraction)
 train_set, val_set = data_provider.train_validation_split(args.max_time_steps)
 
-
-Model = LstmModel(max_time_steps=train_set.max_num_ans,
+model = LstmModel(max_time_steps=train_set.max_num_ans,
                   feature_len=train_set.encoding_dim,
                   n_distinct_questions=train_set.max_prob_set_id,
                   var_dropout=args.var_dropout,
                   batch_size=args.batch)
 
 print('Experiment started at', START_TIME)
-print("Building model...")
-Model.build_graph(n_hidden_units=args.num_hidden_units,
-                  n_hidden_layers=args.num_hidden_layers,
+
+model.build_graph(n_hidden_units=args.num_hidden_units,
                   clip_norm=args.clip_norm,
-                  # add_gradient_noise=args.add_gradient_noise,
                   optimisation=args.optimisation)
-print("Model built!")
 
 train_saver = tf.train.Saver()
 valid_saver = tf.train.Saver()
 
 with tf.Session() as sess:
-    merged_loss = tf.summary.merge(Model.summary_loss)
-    merged_aucacc = tf.summary.merge(Model.summary_aucacc)
+    merged_loss = tf.summary.merge(model.summary_loss)
+    merged_aucacc = tf.summary.merge(model.summary_aucacc)
     train_writer = tf.summary.FileWriter(SAVE_DIR + '/train', graph=sess.graph)
     valid_writer = tf.summary.FileWriter(SAVE_DIR + '/valid', graph=sess.graph)
     sess.run(tf.global_variables_initializer())
@@ -131,80 +120,55 @@ with tf.Session() as sess:
 
     print("Starting training...")
     for epoch in range(args.epochs):
-        # Train one epoch!
-        sess.run(Model.auc_init)
-        sess.run(Model.acc_init)
-        total_sum = 0
-        total_num = 0
-        learning_rate = get_learning_rate(epoch,
-                                          args.init_learn_rate,
-                                          args.min_learn_rate,
-                                          args.lr_exp_decay,
-                                          args.lr_decay_step)
-        for i, (inputs, targets, target_ids) in enumerate(train_set):
-            _, loss, acc_update, auc_update, summary_loss,logit_list = sess.run(
-                [Model.training, Model.loss, Model.accuracy[1], Model.auc[1],
-                 merged_loss,Model.logit_list],
-                feed_dict={Model.inputs: inputs,
-                           Model.targets: targets,
-                           Model.target_ids: target_ids,
-                           Model.learning_rate: learning_rate,
-                           Model.keep_prob: float(args.keep_prob)})
+        model.reuse = False
+        sess.run(model.auc_init)
+        sess.run(model.acc_init)
+        learning_rate = get_learning_rate(epoch, args.init_learn_rate, args.min_learn_rate,
+                                          args.lr_exp_decay, args.lr_decay_step)
 
+        for i, (inputs, targets, target_ids) in enumerate(train_set):
+            _, loss, acc_update, auc_update, summary_loss = sess.run(
+                [model.training, model.loss, model.accuracy[1], model.auc[1],
+                 merged_loss],
+                feed_dict={model.inputs: inputs,
+                           model.targets: targets,
+                           model.target_ids: target_ids,
+                           model.learning_rate: learning_rate,
+                           model.keep_prob: float(args.keep_prob)})
 
             if args.log_stats and epoch % 10 == 0 and i == 0:
-                # optional logging for debugging.
-                print("learning rate is: {}".format(learning_rate))
-                for gv in Model.grads_and_vars:
-                    _ = sess.run([tf.Print(gv, [tf.norm(gv[0]), gv[1].name],
-                                           message="Grad norm is: ")],
-                                 feed_dict={Model.inputs: inputs,
-                                            Model.targets: targets,
-                                            Model.target_ids: target_ids,
-                                            Model.learning_rate: learning_rate,
-                                            Model.keep_prob: float(args.keep_prob)})
+                log_learning_rate_and_grad_norms(sess, model, inputs, targets,
+                                                 target_ids, learning_rate, args.keep_prob)
 
         accuracy, auc, summary_aucacc = sess.run(
-            [Model.accuracy[0], Model.auc[0], merged_aucacc],
-            feed_dict={Model.inputs: inputs,
-                       Model.targets: targets,
-                       Model.target_ids: target_ids})
-        print(
-            "Epoch {},  Loss: {:.3f},  Accuracy: {:.3f},  AUC: {:.3f} (train)"
-                .format(epoch, loss, accuracy, auc))
+            [model.accuracy[0], model.auc[0], merged_aucacc])
 
+        print("Epoch {},  Loss: {:.3f},  Accuracy: {:.3f},  AUC: {:.3f} (train)"
+              .format(epoch, loss, accuracy, auc))
 
+        # save metrics and model each epoch
         train_writer.add_summary(summary_loss, epoch)
         train_writer.add_summary(summary_aucacc, epoch)
-
-        # save model each epoch
         save_file = "{}/{}_{}.ckpt".format(SAVE_DIR, args.name, epoch)
         train_saver.save(sess, save_file)
 
-        sess.run(Model.auc_init)
-        sess.run(Model.acc_init)
-
-        # Compute metrics on validation set (no training)
-        loss_total = 0
-        accuracy_total = 0
-        auc_total = 0
+        # Evaluate on validation set
+        model.reuse = True
+        sess.run(model.auc_init)
+        sess.run(model.acc_init)
 
         for i, (inputs, targets, target_ids) in enumerate(val_set):
             loss, acc_update, auc_update, summary_loss = sess.run(
-                [Model.loss, Model.accuracy[1], Model.auc[1], merged_loss],
+                [model.loss, model.accuracy[1], model.auc[1], merged_loss],
                 feed_dict={
-                    Model.inputs: inputs,
-                    Model.targets: targets,
-                    Model.target_ids: target_ids})
+                    model.inputs: inputs,
+                    model.targets: targets,
+                    model.target_ids: target_ids})
 
-            accuracy, auc, summary_aucacc = sess.run(
-                [Model.accuracy[0], Model.auc[0], merged_aucacc],
-                feed_dict={Model.inputs: inputs,
-                           Model.targets: targets,
-                           Model.target_ids: target_ids,
-                           Model.keep_prob: 1.0})
+        accuracy, auc, summary_aucacc = sess.run(
+            [model.accuracy[0], model.auc[0], merged_aucacc])
         print("Epoch {},  Loss: {:.3f},  Accuracy: {:.3f},  AUC: {:.3f} (valid)"
-                .format(epoch,loss, accuracy, auc))
+              .format(epoch, loss, accuracy, auc))
 
         valid_writer.add_summary(summary_loss, epoch)
         valid_writer.add_summary(summary_aucacc, epoch)
@@ -214,40 +178,4 @@ with tf.Session() as sess:
 
     print("Saved model at", save_file)  # training finished
 
-    # Get and save loss, accuracy, and auc metrics
-    events_file_train = get_events_filepath(SAVE_DIR, 'train')
-    metrics_train = events_to_numpy(events_file_train)
-    np.save(os.path.join(SAVE_DIR, 'metrics_train'), metrics_train)
-
-    events_file_valid = get_events_filepath(SAVE_DIR, 'valid')
-    metrics_valid = events_to_numpy(events_file_valid)
-    np.save(os.path.join(SAVE_DIR, 'metrics_valid'), metrics_valid)
-
-    # plot metrics
-    e = np.arange(args.epochs)
-    plt.figure()
-    train_plt, = plt.plot(e, metrics_train[:, 0])
-    valid_plt, = plt.plot(e, metrics_valid[:, 0])
-    plt.legend([train_plt, valid_plt], ['train', 'valid'])
-    plt.xlabel('Epoch')
-    plt.ylabel('loss')
-    plt.title('Loss per epoch')
-    plt.savefig(SAVE_DIR + '/loss.png')
-
-    plt.figure()
-    train_plt, = plt.plot(e, metrics_train[:, 1])
-    valid_plt, = plt.plot(e, metrics_valid[:, 1])
-    plt.legend([train_plt, valid_plt], ['train', 'valid'])
-    plt.xlabel('Epoch')
-    plt.ylabel('AUC')
-    plt.title('AUC per epoch')
-    plt.savefig(SAVE_DIR + '/auc.png')
-
-    plt.figure()
-    train_plt, = plt.plot(e, metrics_train[:, 2])
-    valid_plt, = plt.plot(e, metrics_valid[:, 2])
-    plt.legend([train_plt, valid_plt], ['train', 'valid'])
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.title('Accuracy per epoch')
-    plt.savefig(SAVE_DIR + '/accuracy.png')
+plot_learning_curves(SAVE_DIR, args.epochs)